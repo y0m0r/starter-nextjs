42af6fd103bf121250edb1a7e91454ee095cd804
Yuhei Omori <yuheiomori@gmail.com>
2022-09-26 17:50:53 +0900

    update

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -14,34 +12,9 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Hello, <a href="https://www.cyclic.sh/">Cyclic</a> with <a href="https://nextjs.org">Next.js!</a>
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <p className={styles.description}>
-          Try out api routes{' '}
-          <Link href="api/hello" passHref>
-            <code className={styles.code}>api/hello</code>
-          </Link>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-        </div>
       </main>
-
       <footer className={styles.footer}>
         <a
           href="https://app.cyclic.sh"
